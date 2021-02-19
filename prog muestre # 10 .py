4fe449f4f9fcbcbf9ebabe46214f1ba59254582f
KingLR04 <79029839+KingLR04@users.noreply.github.com>
2021-02-19 09:42:07 -0400

    Add files via upload

---
+++ prog muestre # 10 .py
@@ -0,0 +1,43 @@
+
+print( "programa que pida ingrear un # 0 al 9")
+
+opcion = 0
+
+while opcion !=1 :
+    numero = int(input("Ingrese numero : "))
+
+    if numero == 0:
+        print("cero")
+
+    elif numero ==1:
+        print("uno")
+
+    elif numero ==2:
+        print("2")
+
+    elif numero == 3:
+      print("tres")
+
+    elif numero == 4:
+        print("4")
+
+    elif numero == 5:
+        print("5")
+
+    elif numero == 6:
+        print("6")
+
+    elif numero == 7:
+        print("7")
+
+    elif numero == 8:
+        print("8")
+
+    elif numero == 9:
+        print("9")
+        break
+print("fin.")
+
+
+
+
---
+++ programa pida ingresar # 0 al 9 .py
@@ -0,0 +1,12 @@
+
+print( "programa que muestre la letra Z  a la A ")
+
+Z = 97
+A = 122
+while z <= a:
+    print(chr(z))
+    z= z-1
+
+
+
+
